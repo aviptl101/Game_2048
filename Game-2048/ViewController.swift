36abdf24c92971a6315d6d5b6b95b19b3739314f
Avinash <avinash.patel@fivebelow.com>
2021-07-16 13:18:59 +0530

    Initial Commit

---
+++ Game-2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game-2048
+//
+//  Created by Avinash on 16/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
