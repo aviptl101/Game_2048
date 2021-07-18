4d280fadcb55f528b634e60031bd19643fc4b64a
Avinash <avinash.patel@fivebelow.com>
2021-07-18 23:45:03 +0530

    Added steps in squareView to calculate number of moves for respective Tile

--- Game-2048/Views/SquareView.swift
+++ Game-2048/Views/SquareView.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
+/// SquareView is place holder for Tile and holds corresponding Tile's value
+/// SquareViews are used to calculate Tile's moves and Tile's actions like getting Merged Or Removed
 class SquareView: UIView {
     var position = (row: -1, col: -1)
     var value = 0
+    // Number of steps respective Tile will move after swipeAction
+    var steps = 0
 
     override init(frame: CGRect) {
         super.init(frame: frame)
