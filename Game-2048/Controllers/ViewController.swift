0edcee54d4cd9b497a487c4d4718a0f348558395
Avinash <avinash.patel@fivebelow.com>
2021-07-18 13:47:34 +0530

    BoardView setup with storyboard

--- Game-2048/Controllers/ViewController.swift
+++ Game-2048/Controllers/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var boardView: UIView!
+    var boardVM: GameViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        boardVM = GameViewModel(dimension: 4, view: boardView)
     }
-
-
 }
 
