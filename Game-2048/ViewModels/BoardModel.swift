0d3be4188519ca0e0d779d28866aa1e6e127ba70
Avinash <avinash.patel@fivebelow.com>
2021-07-18 22:11:03 +0530

    Added variables in BoardModel to get adjacent squares from given position square

--- Game-2048/ViewModels/BoardModel.swift
+++ Game-2048/ViewModels/BoardModel.swift
@@ -7,12 +7,18 @@
 
 import Foundation
 
+/// BoardModel holds and maintains all the board squares and their data and represents Game state and Board status
+/// Squares are place holder for Tiles and represents corresponding Tile's value
 class BoardModel {
     var dimension = 4
+    // Set curPosition to get values or adjacent squares of a square at particular position
     var curPosition = (row: 0, col: 0)
-    
     var squaresList = [[SquareView]]()
 
+    var posValue: Int {
+        return squaresList[curPosition.row ][curPosition.col].value
+    }
+    
     var curSquare: SquareView? {
         return squaresList[curPosition.row][curPosition.col]
     }
@@ -20,5 +26,45 @@ class BoardModel {
     init(dimension: Int) {
         self.dimension = dimension
         squaresList = [[SquareView]](repeating: [SquareView](repeating: SquareView(), count: dimension), count: dimension)
+    }
+    
+    func getSquare(for position: (row: Int, col: Int)) -> SquareView? {
+        if position.row >= 0 && position.row < dimension && position.col >= 0 && position.col < dimension {
+            return squaresList[position.row][position.col]
+        } else {
+            return nil
+        }
+    }
+    
+    var leftSquare: SquareView? {
+        if curPosition.col > 0 {
+            return squaresList[curPosition.row][curPosition.col - 1]
+        } else {
+            return nil
+        }
+    }
+    
+    var rightSquare: SquareView? {
+        if curPosition.col < dimension - 1 {
+            return squaresList[curPosition.row][curPosition.col + 1]
+        } else {
+            return nil
+        }
+    }
+    
+    var upSquare: SquareView? {
+        if curPosition.row > 0 {
+            return squaresList[curPosition.row - 1][curPosition.col]
+        } else {
+            return nil
+        }
+    }
+    
+    var downSquare: SquareView? {
+        if curPosition.row < dimension - 1 {
+            return squaresList[curPosition.row + 1][curPosition.col]
+        } else {
+            return nil
+        }
     }
 }
